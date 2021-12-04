7acf2382932521b0366b31d9eed9fb7d25f2b6a3
Yuxiang Gao <yuxiang.gao@jhu.edu>
2021-12-03 21:12:59 -0500

    Clean controller

--- src/pages/Controller.vue
+++ src/pages/Controller.vue
@@ -1,97 +1,98 @@
 <template>
   <div class="q-pad-md row justify-center">
-    <q-card
-      v-touch-pan.prevent.mouse="handlePan"
-      class="custom-area cursor-pointer bg-primary text-white shadow-2 relative-position row flex-center"
-    >
-      <div v-if="info" class="custom-info">
-        <pre>{{ info }}</pre>
-      </div>
-      <div v-else class="text-center">
-        <q-icon name="arrow_upward" />
-        <div class="row items-center">
-          <q-icon name="arrow_back" />
-          <div>Pan in any direction</div>
-          <q-icon name="arrow_forward" />
+    <q-card v-touch-pan.prevent.mouse="handlePan" class="flex-center custom-area">
+      <!-- class="custom-area cursor-pointer bg-primary text-white shadow-2 relative-position row flex-center" -->
+      <q-card-section
+        class="full-width bg-primary text-white row flex-center cursor-pointer touch-area"
+      >
+        <div v-if="state.info" class="custom-info">
+          <pre>{{ state.info }}</pre>
         </div>
-        <q-icon name="arrow_downward" />
-      </div>
+        <div v-else class="text-center">
+          <q-icon name="arrow_upward" />
+          <div class="row items-center">
+            <q-icon name="arrow_back" />
+            <div>Pan in any direction</div>
+            <q-icon name="arrow_forward" />
+          </div>
+          <q-icon name="arrow_downward" />
+        </div>
 
-      <div v-show="panning" class="touch-signal">
-        <q-icon name="touch_app" />
-      </div>
+        <div v-show="state.panning" class="touch-signal">
+          <q-icon name="touch_app" />
+        </div>
+      </q-card-section>
+      <q-separator />
+
+      <q-card-actions align="right">
+        <q-input rounded standout dense v-model="state.cmdVelTopic" label="cmd_vel topic"></q-input>
+        <!-- <q-btn flat>Action 1</q-btn> -->
+      </q-card-actions>
     </q-card>
   </div>
-  <q-input rounded outlined v-model="cmdVelTopic" label="cmd_vel topic"></q-input>
 </template>
 
 <script setup>
 import { rosInterface } from 'src/utils/RosUtils'
-import { ref, watch } from 'vue'
+import { toRefs, ref, reactive, watch, computed, onMounted } from 'vue'
 
-const info = ref(null)
-const panning = ref(false)
-const cmdVelTopic = ref(rosInterface.cmdVelPublisher.name)
+const state = reactive({
+  info: null,
+  panning: false,
+  direction: "stop",
+  cmdVelTopic: rosInterface.cmdVelPublisher.name,
+  velocity: 0.5,
+  cmdVelMsg: computed(() => {
+    var msg = {
+      up: { linear: { x: state.velocity } },
+      down: { linear: { x: -state.velocity } },
+      left: { angular: { x: state.velocity } },
+      right: { angular: { x: -state.velocity } },
+      stop: { linear: { x: 0.0 }, angular: { x: 0.0 } }
+    };
+    return msg[state.direction];
+  })
+})
+
 function handlePan({ evt, ...newInfo }) {
-  info.value = newInfo
+  state.info = newInfo
 
   // native Javascript event
   // console.log(evt)
 
   if (newInfo.isFirst) {
-    panning.value = true
+    state.panning = true
   }
   else if (newInfo.isFinal) {
-    panning.value = false
+    state.panning = false
+    state.direction = "stop"
   }
 
-  var msg = {
-    linear:
-    {
-      x: 0.0,
-      y: 0.0,
-      z: 0.0
-    },
-    angular: {
-      x: 0.0,
-      y: 0.0,
-      z: 0.0
-    }
-  }
-  if (newInfo.isFinal) {
-    rosInterface.cmdVelPublisher.publish(msg)
-  }
-  else {
-    switch (newInfo.direction) {
-      case "up":
-        msg["linear"]["x"] = 0.5;
-        break;
-      case "down":
-        msg["linear"]["x"] = -0.5;
-        break;
-      case "left":
-        msg["angular"]["x"] = 0.1;
-        break;
-      case "right":
-        msg["angular"]["x"] = -0.1;
-        break;
-    }
-    rosInterface.cmdVelPublisher.publish(msg)
-  }
-
-
+  state.direction = newInfo.direction
 }
 
 // watch topic and change publisher on change
-watch(cmdVelTopic, (topic) => { rosInterface.cmdVelPublisher.name = topic; })
+watch(() => state.cmdVelTopic, (topic) => { rosInterface.cmdVelPublisher.name = topic; })
+// Publish 10Hz
+onMounted(() => {
+  setInterval(() => {
+    if (state.panning) {
+      // console.log("pub message")
+      rosInterface.cmdVelPublisher.publish(state.cmdVelMsg)
+    }
+  }, 100)
+})
 </script>
 
 <style lang="sass" scoped>
 .custom-area
   width: 90%
-  height: 480px
+  height: 550px
   border-radius: 3px
-  padding: 8px
+  padding: 0px
+
+.touch-area
+  height: 85%
 
 .custom-info pre
   width: 180px
