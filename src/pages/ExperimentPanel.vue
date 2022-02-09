42ed4cacdff80400e27d3bbe1843bf70ab05af50
Yuxiang Gao <yuxiang.gao@jhu.edu>
2022-02-09 17:13:10 -0500

    Add tts interface

--- src/pages/ExperimentPanel.vue
+++ src/pages/ExperimentPanel.vue
@@ -5,8 +5,17 @@
       <div class="q-gutter-sm">
         <div>Task: <strong>current</strong></div>
         <q-btn-group rounded>
-          <q-btn rounded color="primary" label="Start Task" />
-          <q-btn rounded color="red" label="Stop Task" />
+          <q-btn
+            rounded
+            color="primary"
+            label="Toggle Task"
+            @click="
+              () => {
+                triggerServiceByName('/clean_surface/trigger_clean_surface');
+              }
+            "
+          />
+          <!-- <q-btn rounded color="red" label="Stop Task" /> -->
         </q-btn-group>
         <div>
           Facial expression: <strong>{{ lastExpression }}</strong>
@@ -28,12 +37,68 @@
 
         <div>Voice Command</div>
         <q-list bordered separator style="max-width: 350px">
-          <q-item clickable v-ripple>
-            <q-item-section>
-              <q-item-label overline>Voice</q-item-label>
-              <q-item-label>Hello</q-item-label>
-            </q-item-section>
-          </q-item>
+          <template v-for="(text, index) in speechList" :key="index">
+            <q-item clickable v-ripple>
+              <q-item-section
+                @click="
+                  () => {
+                    speak(ttsService, text);
+                  }
+                "
+              >
+                <q-item-label overline>Voice</q-item-label>
+                <q-item-label>{{ text }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </template>
+          <q-input outlined dense v-model="speechText">
+            <template v-slot:append>
+              <q-btn
+                round
+                dense
+                color="primary"
+                icon="mic"
+                @click="
+                  () => {
+                    speak(ttsService, speechText);
+                  }
+                "
+              />
+            </template>
+          </q-input>
+        </q-list>
+
+        <div>Machine-like Voice Command</div>
+        <q-list bordered separator style="max-width: 350px">
+          <template v-for="(text, index) in machineSpeechList" :key="index">
+            <q-item clickable v-ripple>
+              <q-item-section
+                @click="
+                  () => {
+                    speak(ttsMachineService, text);
+                  }
+                "
+              >
+                <q-item-label overline>Machine Voice</q-item-label>
+                <q-item-label>{{ text }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </template>
+          <q-input outlined dense v-model="machineText">
+            <template v-slot:append>
+              <q-btn
+                round
+                dense
+                color="primary"
+                icon="mic"
+                @click="
+                  () => {
+                    speak(ttsMachineService, machineText);
+                  }
+                "
+              />
+            </template>
+          </q-input>
         </q-list>
 
         <div>
@@ -52,19 +117,68 @@
 
 <script setup>
 import { toRefs, ref, reactive, watch, watchEffect, computed, onMounted } from "vue";
-import { rosConnection } from "src/utils/RosUtils";
+import { rosConnection, triggerServiceByName } from "src/utils/RosUtils";
 
 const expressionList = ["happy", "sad", "angry", "focused", "confused"];
 const lastExpression = ref("None");
+
+const speechText = ref("");
+const machineText = ref("");
+
+const speechList = [
+  "Hello, my name is Stretch.",
+  "You should sort this by shape.",
+  "You should sort this by color.",
+  "You should sort this by number.",
+  "I'm not sure.",
+  "Would you excuse me for a moment?",
+  "Yes, how can I help you?",
+  "Sorry, I don't know.",
+];
+
+const machineSpeechList = [
+  "Hello",
+  "Shape",
+  "Color",
+  "Number",
+  "Occupied.",
+  "I don't know.",
+];
 var expressionTopic = new ROSLIB.Topic({
   ros: rosConnection.ros,
-  name: "/face",
+  name: "/robot_face/express",
   messageType: "std_msgs/String",
+});
+
+var ttsService = new ROSLIB.Service({
+  ros: rosConnection.ros,
+  name: "/tts",
+  serviceType: "sp_msgs/Speech",
+});
+
+var ttsMachineService = new ROSLIB.Service({
+  ros: rosConnection.ros,
+  name: "/tts_machine",
+  serviceType: "sp_msgs/Speech",
 });
 
 function express(expressionType) {
   var expressionMsg = new ROSLIB.Message({ data: expressionType });
   expressionTopic.publish(expressionMsg);
   lastExpression.value = expressionType;
+}
+
+function speak(client, text) {
+  var request = new ROSLIB.ServiceRequest({ data: text });
+  client.callService(request, function (result) {
+    console.log(
+      "Result for service call on " +
+        client.name +
+        ": " +
+        result.success +
+        " - " +
+        result.message
+    );
+  });
 }
 </script>
